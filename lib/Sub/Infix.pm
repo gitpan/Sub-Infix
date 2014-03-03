2747b0ac27f86a5c7bf3ef488e07aebbd55b9a9d
Toby Inkster <tobyink@cpan.org>
2014-03-03 22:07:02 +0000

    Import of TOBYINK/Sub-Infix-0.003 from CPAN.
    gitpan-cpan-distribution: Sub-Infix
    gitpan-cpan-version:      0.003
    gitpan-cpan-path:         TOBYINK/Sub-Infix-0.003.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

--- lib/Sub/Infix.pm
+++ lib/Sub/Infix.pm
@@ -6,10 +6,11 @@ use warnings;
 
 BEGIN {
 	$Sub::Infix::AUTHORITY = 'cpan:TOBYINK';
-	$Sub::Infix::VERSION   = '0.002';
+	$Sub::Infix::VERSION   = '0.003';
 }
 
-use base "Exporter";
+use Exporter ();
+our @ISA    = qw( Exporter );
 our @EXPORT = qw( infix );
 
 sub infix (&)
@@ -159,7 +160,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2013 by Toby Inkster.
+This software is copyright (c) 2013-2014 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
